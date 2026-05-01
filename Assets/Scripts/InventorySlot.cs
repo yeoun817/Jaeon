da446f22a1cbbc491e6f6a205c7df615f4aebb79
yeoun817 <yeoun817@gmail.com>
2026-05-02 01:50:31 +0900

    인벤토리 기능
    좀 이상하게 해서 한번 갈아엎음

---
+++ Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEditor.EventSystems;
+using UnityEngine.EventSystems;
+
+public class InventorySlot : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        if(transform.childCount == 0)
+        {
+            InventoryItem inventoryItem = eventData.pointerDrag.GetComponent<InventoryItem>();
+            inventoryItem.parentAfterDrag = transform;
+        }
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(menuName = "Scriptable Object/Item")]
+public class Item : ScriptableObject 
+{
+    [Header("Only gameplay")]
+    public TileBase tile;
+    public ItemType type;
+    public ActionType actionType;
+    public Vector2Int range = new Vector2Int(5, 4);
+
+    [Header("Only UI")]
+    public bool stackable;
+
+    [Header("Both")]
+    public Sprite image;
+
+}
+
+public enum ItemType
+{
+    Material,
+    Tool,
+    Armor,
+    Weapon
+}
+
+public enum ActionType
+{
+    non,
+    Plant,
+    Harvest,
+    PutOn,
+    Attack
+}
